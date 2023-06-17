a7da3fa7c4a24956273d35c369263e76c06d7e68
aswati94 <aswatistudy@gmail.com>
2023-06-17 19:28:39 +0530

    new

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+console.log("zig zag assignment");
+ //   ----> \ this is the escape character it escapes the character & we have to Escaping the escape character 
+console.log("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\"); 
+
+console.log("hello "+'swati '+`How? `+"are "+'you? '+`What `+"are "+'you '+`doing`); 
+console.log("swati "+ 20);
+console.log('abhi '+30);
+console.log(`devansh `+40);
+console.log(10+ "Tanmay");
+console.log(30+ 'bhavya');
+console.log(20+ `samarth`);
+console.log("30"+'20'+`10`);    
+console.log(30+20);
